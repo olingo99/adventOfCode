030a163c0f3feeaf2e65504c257db23ad55ef094
Olingo <195367@ecam.be>
2022-12-01 17:43:28 +0100

    done

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use std::collections::HashMap;
-
+use std::time::{Duration, Instant};
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
@@ -12,8 +12,11 @@ where P: AsRef<Path>, {
 
 
 fn main() {
+    let start = Instant::now();
     let mut calories = Vec::new();
     calories.push(Vec::new());
+
+    let mut res = Vec::new();
 
     let mut id = 0;
     if let Ok(lines) = read_lines("../data/CalorieCounting.txt") {
@@ -32,12 +35,14 @@ fn main() {
         }
     }
 
-    //let mut res: Vec<u32> = Vec::new();
-    let mut res:u32 = 0;
-    for elf in calories{
-        res = elf.iter().sum();
-    }   
-    dbg!(res);
+    res = calories.iter().map(|x| x.iter().sum()).collect::<Vec<u32>>();
+
+    res.sort();
+
+    let duration = start.elapsed();
+    let l = res.len();
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
+    dbg!(res[l-1]+res[l-2]+res[l-3]);
     
 
 }
