b8609e8598c480a08212d1e887f6f136acc6a62b
Olingo <195367@ecam.be>
2022-12-01 12:22:42 +0100

    t

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::collections::HashMap;
+
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut calories = Vec::new();
+    calories.push(Vec::new());
+
+    let mut id = 0;
+    if let Ok(lines) = read_lines("../data/CalorieCounting.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(ip) = line {
+
+                if ip.is_empty() {
+                    calories.push(Vec::new());
+                    id+=1;
+                }
+                else{
+                    calories[id].push(ip);
+                }
+            }
+        }
+    }
+
+    
+    
+
 }
