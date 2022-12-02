f45713ba323b5d5d09833f429add33ae50ce1f54
olingo99 <engels.theo@skynet.be>
2022-12-02 17:47:13 +0100

    day2

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,66 @@
 // use std::fs::File;
 // use std::io::{self, BufRead};
 // use std::path::Path;
-// use std::collections::HashMap;
-// use std::time::{Duration, Instant};
-
-// fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-// where P: AsRef<Path>, {
-//     let file = File::open(filename)?;
-//     Ok(io::BufReader::new(file).lines())
-// }
-
-
-// fn main() {
-//     let mut score : u32 = 0;
-//     let mut enemy : u32 = 0;
-//     let mut player : u32 = 0;
-//     let mut split;
-//     if let Ok(lines) = read_lines("../data/rpc.txt") {
-//         // Consumes the iterator, returns an (Optional) String
-//         for line in lines {
-//             if let Ok(ip) = line {
-//                 split = ip.split(" ").collect::<Vec<&str>>();
-//                 dbg!(split);
-//                 // enemy = split[0].parse::<u32>().unwrap();
-//                 // player = split[1].parse::<u32>().unwrap();
-//                 // score += player + player;
-//             }
-//         }
-//     }
-
-//     dbg!(2%3);
-    
-
-// }
+use std::collections::HashMap;
+use std::time::{Duration, Instant};
 
 
 use std::io::{BufRead, BufReader};
 use std::fs::File;
 fn main(){
+    let start = Instant::now();
     let mut score : u32 = 0;
-    let mut enemy : u32 = 0;
-    let mut player : u32 = 0;
     let reader = BufReader::new(File::open("../data/rpc.txt").expect("Cannot open file.txt"));
-
+    let map : HashMap<String, u32> = HashMap::from([
+        ("A X".to_string(), 3),
+        ("A Y".to_string(), 4),
+        ("A Z".to_string(), 8),
+        ("B X".to_string(), 1),
+        ("B Y".to_string(), 5),
+        ("B Z".to_string(), 9),
+        ("C X".to_string(), 2),
+        ("C Y".to_string(), 6),
+        ("C Z".to_string(), 7),
+    ]);
     for line in reader.lines() {
-        let words = line.as_ref().unwrap().split(" ").collect::<Vec<&str>>();
-        enemy = translate(words[0]);
-        player = translate(words[1]);
-        dbg!(enemy);
-        dbg!(player);
+
+        score += map.get(&line.unwrap()).unwrap();
     }
+    let duration = start.elapsed();
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
+    dbg!(score);
 }
 
 fn translate(word: &str ) -> u32{
     match word {
-        "A" => 1,
-        "B" => 2,
-        "C" => 3,
-        "X" => 1,
-        "Y" => 2,
-        "Z" => 3,
+        "X" => 0,
+        "Y" => 3,
+        "Z" => 6,
         _ => 0,
     }
+}
+
+fn exo1(){
+    let map : HashMap<String, u32> = HashMap::from([
+        ("A X".to_string(), 4),
+        ("A Y".to_string(), 8),
+        ("A Z".to_string(), 3),
+        ("B X".to_string(), 1),
+        ("B Y".to_string(), 5),
+        ("B Z".to_string(), 9),
+        ("C X".to_string(), 7),
+        ("C Y".to_string(), 2),
+        ("C Z".to_string(), 6),
+    ]);
+    let mut score : u32 = 0;
+    // let mut enemy : u32 = 0;
+    // let mut player : u32 = 0;
+    let reader = BufReader::new(File::open("../data/rpc.txt").expect("Cannot open file.txt"));
+
+    for line in reader.lines() {
+        //score += map.get(line.unwrap()).unwrap();
+        //dbg!(line.unwrap());
+        score += map.get(&line.unwrap()).unwrap();
+    }
+    dbg!(score);
 }
