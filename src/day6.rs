171a474de61b04a7292aa70afe5f61653149a421
Olingo <195367@ecam.be>
2022-12-06 16:04:27 +0100

    day6

--- src/day6.rs
+++ src/day6.rs
@@ -1,23 +1,22 @@
 use crate::input;
-//use std::collections::HashMap;
+use std::collections::HashSet;
 
 pub fn day6() -> input::Result<()> {
     let contents = input::load_day_file("day6.txt");
+    let window_size = 14;
     
-    let mut circular_buffer = Vec::new();
-    let mut current_position = 0;
-
-    for (i,cha) in contents.chars().enumerate() {
-        // if circular_buffer.len()>=3 && cha not in circular_buffer {
-        //     let res = 
-        //     break;
-        // }
-        circular_buffer.insert(current_position, cha);
-        current_position += 1;
-        dbg!(i);
-        dbg!(cha);
+    for (i,window) in contents.chars().collect::<Vec<char>>().windows(window_size).enumerate() {
+        let mut set = HashSet::new();
+        for elem in window {
+            set.insert(elem);
+        }
+        if set.len() == window_size {
+            dbg!(i+window_size);
+            break;
+        }
     }
 
 
+    //dbg!(res);
     Ok(())
 }
