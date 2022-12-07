afed659429537c827c4b652b411b70c3c89c7be1
Olingo <195367@ecam.be>
2022-12-07 23:44:20 +0100

    prep speedrun day8

---
+++ src/day8.rs
@@ -0,0 +1,8 @@
+use crate::input;
+use std::collections::HashMap;
+
+pub fn day8() -> input::Result<()> {
+    let contents = input::load_day_file("day8.txt");
+    dbg!(contents);
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@
 // mod day4;
 //mod day5;
 //mod day6;
-mod day7;
+//mod day7;
+mod day8;
 
 mod input;
 
@@ -18,8 +19,8 @@ fn main() {
         // Day::new("Day 4", day4::day4),
         //Day::new("Day 5", day5::day5),
         //Day::new("Day 6", day6::day6),
-        Day::new("Day 7", day7::day7),
-        // Day::new("Day 8", day8::day8),
+        //Day::new("Day 7", day7::day7),
+        Day::new("Day 8", day8::day8),
         // Day::new("Day 9", day9::day9),
         // Day::new("Day 10", day10::day10),
         // Day::new("Day 11", day11::day11),
