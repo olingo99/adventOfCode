33d5c51a31206905eb40278f2af1efef33b2b72b
olingo99 <engels.theo@skynet.be>
2022-12-13 18:54:21 +0100

    g

--- src/day11.rs
+++ src/day11.rs
@@ -1,33 +1,57 @@
 use crate::input;
 use std::collections::HashMap;
-
-struct Monkey {
+use regex::RegexSet;
+struct Monkey{
     id: u16,
     items: Vec<u32>,
     operation: String,
 }
 
+impl Monkey {
+    fn new(id: u16, items: Vec<u32>, operation: String) -> Monkey{
+        Monkey {
+            id,
+            items,
+            operation,
+        }
+    }
+    fn operation(&mut self){
+        let data = self.operation.split_whitespace().collect::<Vec<&str>>();
+
+        let set = RegexSet::new(&[
+            r"old \* \d+",
+            r"old \* old",
+            r"old \+ \d+",
+        ]).unwrap();
+        dbg!(&self.operation);
+        //let res = set.is_match(&self.operation);
+        let res: Vec<_> = set.matches(&self.operation).into_iter().collect();
+        dbg!(&res);
+        match res[0]{
+            0 => {
+                let num = data[2].parse::<u32>().unwrap();
+                self.items[0] = self.items[0] * num;
+                dbg!(&self.items);
+            },
+            1 => {
+                self.items[0] = self.items[0].pow(2);
+                dbg!(&self.items);
+            },
+            2 => {
+                let num = data[2].parse::<u32>().unwrap();
+                self.items[0] = self.items[0] + num;
+                dbg!(&self.items);
+            },
+            _ => {
+                dbg!("No match");
+            }
+        }
+    }
+}
+
 pub fn day11() -> input::Result<()> {
     let contents = input::load_day_file("day11.txt");
     let mut monkeys = Vec::new();
-    // for line in contents.lines() {
-    //     if line.is_empty() {
-    //         continue;
-    //     }
-    //     let mut parts = line.split_whitespace().collect::<Vec<&str>>();
-    //     match parts[0]{
-    //         "Monkey" => {
-    //             let id = parts[1].parse::<u16>().unwrap();
-    //             let items = parts[2].split(",").map(|x| x.parse::<u32>().unwrap()).collect::<Vec<u32>>();
-    //             let operation = parts[3].to_string();
-    //             let monkey = Monkey{id, items, operation};
-    //             monkeys.push(monkey);
-    //         },
-    //     }
-    //     //if parts[0] == "Monkey"{
-
-    //     }
-    // }
     let people = contents.split( "\r\n\r\n" ).collect::<Vec<&str>>();
     for block in people{
         let lines = block.split("\r\n").collect::<Vec<&str>>();
@@ -36,17 +60,21 @@ pub fn day11() -> input::Result<()> {
         for elem in split{
             temp.push(elem.trim().parse::<u32>().unwrap());
         }
-        monkeys.push(Monkey{
+        let mut monk = Monkey{
             id: monkeys.len() as u16,
-            items: temp.clone(),
-            operation: "ee".to_string(),
-        });
-        // dbg!(&monkeys.last().unwrap().id);
+            items:temp.clone(),
+            operation: lines[2].split("=").collect::<Vec<&str>>().last().unwrap().trim().to_string(),
+        };
+        monkeys.push(monk);
+        let x = monkeys.len();
+        // let mut test = monkeys[x-1];
+        let test = monkeys.last_mut().unwrap();
+        test.operation();
+        let monkey = &mut monkeys[x-1];
+        //monkey.items[0] += 19
+
+        monkey.operation();
         dbg!(&monkeys.last().unwrap().items);
-        // dbg!(&monkeys.last().unwrap().operation);
-        // for line in lines{
-        //     dbg!(line);
-        // }
     }
 
     Ok(())
