029d63131c764bf62506ace016f53c9e9b257f55
Olingo <195367@ecam.be>
2022-12-14 20:49:06 +0100

    d

--- src/day14.rs
+++ src/day14.rs
@@ -1,10 +1,13 @@
 use crate::input;
-use std::collections::HashMap;
+//use std::collections::HashMap;
 
 pub fn day14() -> input::Result<()> {
+    
     let contents = input::load_day_file("day14.txt");
+    let mut map : Vec<Vec<char>> = vec![];
     for line in contents.lines(){
-        
+        let data = line.split(" -> ").collect::<Vec<&str>>();
+        dbg!(data);
     }
     Ok(())
 }
